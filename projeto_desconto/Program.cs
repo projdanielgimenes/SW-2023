9dcb43ca9e3bab5228952244fc9e2e7849236376
Daniel Gimenes <100932065+projdanielgimenes@users.noreply.github.com>
2023-04-04 09:34:54 -0300

    Add files via upload

---
+++ projeto_desconto/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace projeto_desconto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto("ABC", 10);
+        p1.MostraDados();
+    }
+}
---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Pessoa p1 = new Pessoa("Chita",90);
+    //p1.InsereNome("David");
+    //p1.InsereSobreNome("Santos");
+    //p1.InsereIdade(16);
+
+       p1.MostraDados();
+    }
+}
